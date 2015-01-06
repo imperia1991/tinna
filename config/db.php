0b122292e41980df8dbb404c6d81cb27a44ae1ad
Elis Gen <imperia1991@gmail.com>
2015-01-06 17:53:40 +0200

    commit

--- config/db.php
+++ config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=tinna',
-    'username' => 'root',
-    'password' => '',
+    'username' => 'tinna',
+    'password' => 'tinna2015',
     'charset' => 'utf8',
 ];
